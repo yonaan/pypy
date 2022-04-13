0627089aaf90016d60468ba9b55e995f4a5f8899
yonaan <72993275+yonaan@users.noreply.github.com>
2022-04-13 19:25:24 +0900

    Add files via upload

---
+++ tubes/src/tubes/Tubes.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tubes;
+
+/**
+ *
+ * @author Yonaan
+ */
+public class Tubes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Account tim = new Account("Tim Statler", "A00001");
+	tim.showMenu();	
+		
+	Account beyonce = new Account("Beyonce", "B00002");
+	/*beyonce.showMenu();*/
+    }
+    
+}
